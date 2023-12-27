fb72c05f6a4cbdb66f2a84b8c8980398ed67d6d0
Fabio-Frau <fbfr91@yahoo.it>
2023-12-27 15:03:57 +0100

    addinge exercise about user inputs

---
+++ Week 1/python_user_input.py
@@ -0,0 +1,24 @@
+email = input('Please enter your email address: ')
+print(email)
+
+print("Hello", "you", sep =", ")
+
+a = 10
+b = 5
+ans = a + b
+print("Adding the value of {} and {} = {}".format(a,b,ans))
+
+print("I like {0} more than {1}".format("oranges", "grapes"))
+print("I like {1} more than {0}".format("oranges", "grapes"))
+
+
+num1 = input("Please enter the first number: ")
+num2 = input("Please enter a second number: ")
+print(num1, num2)
+print(num1 + num2)
+print(int(num1) + int(num2))
+
+str1 = input("Please enter your first name: ")
+str2 = input("Please enter your last name: ")
+print("Hello " + str1 + " " + str2)
+print("Hello {} {}".format(str1, str2))
