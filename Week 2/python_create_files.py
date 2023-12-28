241ef9372e99c982e272523c6013e5902e83cd10
Fabio-Frau <fbfr91@yahoo.it>
2023-12-28 19:46:32 +0100

    adding exercise about creating files

---
+++ Week 2/python_create_files.py
@@ -0,0 +1,18 @@
+with open("newfile.txt", mode = "w") as file:
+    file.write("This is a new file")
+
+
+with open("newfile2.txt", mode = "w") as file:
+    file.writelines(["This is a secon text file", "This is another line"])
+
+with open("newfile3.txt", mode = "w") as file:
+    file.writelines(["This is a secon text file", "\nThis is another line"])
+
+with open("newfile3.txt", mode = "a") as file:
+    file.writelines(["\nAppending new line", "\nAppending new line"])
+
+try:
+    with open("sample/newfile.txt", "a") as file:
+        file.writelines(["\nAppending new line", "\nAppending new line"])
+except FileNotFoundError as e:
+    print("ERROR", e)
