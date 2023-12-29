aa24926334a53c58f5690631d4b9f9ace031c879
Fabio-Frau <fbfr91@yahoo.it>
2023-12-29 18:52:28 +0100

    adding exercise about importing

---
+++ Week 4/imports.py
@@ -0,0 +1,22 @@
+# import math
+# from math import sqrt
+# import math as m
+# from math import factorial as f
+# from math import factorial, log10, sqrt
+from math import *
+
+
+print("Importing the math module")
+
+# root = math.sqrt(9)
+# root = sqrt(9)
+# print(root)
+
+# cosine = m.cos(0)
+# print(cosine)
+
+# factoria_10 = f(10)
+# print(factoria_10)
+
+x = log10(50)
+print(x)
