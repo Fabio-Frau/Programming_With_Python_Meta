2506c1d2afcd68bf8a8949e660084d3f5480289b
Fabio-Frau <fbfr91@yahoo.it>
2023-12-29 22:41:40 +0100

    adding last exercises

---
+++ Week 4/filechanges.py
@@ -0,0 +1,6 @@
+import os
+
+def print_changes():
+    contents = os.listdir(r"D:\FABIO\INFORMATICA\Programming_With_Python_Meta\Week 4")
+    print("Current directory contents: ")
+    print(contents)
